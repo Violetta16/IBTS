df460463cd39e6bc15706432f5a8634221ab2ea4
Violetta16 <97437517+Violetta16@users.noreply.github.com>
2022-03-10 15:46:30 +0500

    Add files via upload

---
+++ Viola_App/Viola_App/Viola_App.cpp
@@ -0,0 +1,17 @@
+﻿// Viola_App.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include"Dll_Viola.h"
+#include "pch.h"
+#include "framework.h"
+
+#define DLLVIOLA_API __declspec(dllimport MyGroup);
+
+int main()
+{
+	MyGroup();
+	MyName();
+	std::cout << "I have done this!\n";
+}
+
