ae45deef8024719fe3fe5b1d117a1552868ddaef
Violetta16 <97437517+Violetta16@users.noreply.github.com>
2022-03-10 15:38:21 +0500

    Add files via upload

---
+++ Dll_Viola/Dll_Viola/dllmain.cpp
@@ -0,0 +1,19 @@
+﻿// dllmain.cpp : Определяет точку входа для приложения DLL.
+#include "pch.h"
+#include <string>
+#include "Dll_Viola.h"
+
+__declspec(dllexport)
+int __stdcall MyGroup()
+{
+	int a = 571227;
+	return a;
+}
+
+
+__declspec(dllexport)
+std::string MyName()
+{
+	std::string b = "Violetta_Tokareva";
+	return b;
+}
