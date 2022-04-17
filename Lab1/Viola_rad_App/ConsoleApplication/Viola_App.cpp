ee9faa31588f1217a98958ae2b68515a10ee5d34
Violetta16 <violettapovetina88501@gmail.com>
2022-04-17 19:22:44 +0500

    Add ConsoleApplication

---
+++ Lab1/Viola_rad_App/ConsoleApplication/Viola_App.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <tchar.h>
+#include <Dll_rad_Viola.h>
+
+using namespace std;
+int _tmain(int argc, _TCHAR* argv[])
+{
+	char *name;
+   cout<<MyName(name)<<endl;
+   cout<<MyGroup()<<endl;
+
+
+}
