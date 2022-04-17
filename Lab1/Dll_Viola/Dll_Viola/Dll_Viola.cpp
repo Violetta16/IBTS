e66b79d2d32839e8d34adb79f7f70c061200459c
Violetta16 <violettapovetina88501@gmail.com>
2022-04-17 20:34:44 +0500

    Move folders

---
+++ Lab1/Dll_Viola/Dll_Viola/Dll_Viola.cpp
@@ -0,0 +1,22 @@
+﻿// Dll_Viola.cpp : Определяет экспортируемые функции для DLL.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "Dll_Viola.h"
+
+
+// Пример экспортированной переменной
+DLLVIOLA_API int nDllViola=0;
+
+// Пример экспортированной функции.
+DLLVIOLA_API int fnDllViola(void)
+{
+    return 0;
+}
+
+// Конструктор для экспортированного класса.
+CDllViola::CDllViola()
+{
+    return;
+}
---
+++ Lab1/Viola_App/Viola_App.cpp
@@ -0,0 +1,16 @@
+﻿// Viola_App.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include"Dll_Viola.h"
+#include "pch.h"
+#include "framework.h"
+
+#define DLLVIOLA_API __declspec(dllimport MyGroup);
+
+int main()
+{
+	MyGroup();
+	std::cout << "I have done this!\n";
+}
+
